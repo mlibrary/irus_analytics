9cd5dffb97d1ab8e2102433a2727b8a7e55fff49
Fritz Freiheit <fritx@umich.edu>
2021-04-01 15:41:45 -0400

    BLUEDOC-1101 - Pull updates from DBD to IRUS analytics gem - 2

--- lib/irus_analytics/configuration.rb
+++ lib/irus_analytics/configuration.rb
@@ -7,13 +7,13 @@ module IrusAnalytics
   class Configuration
     include ::ConfigFiles
     config_directories etc: [File.join(__dir__, '..', '..', 'config')]
-    static_config_files :irus_config
+    static_config_files :irus_analytics_config
     ::Configurations::MockRails.(self.directories[:etc]) if !defined?(Rails)
 
     class << self
       [:source_repository, :irus_server_address, :robots_file].each do |name|
         define_method name do
-          irus_config[Rails.env][name.to_s]
+          irus_analytics_config[Rails.env][name.to_s]
         end
       end
 
