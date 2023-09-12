42b72d2d15a8baf13add7e5547df10effa164654
Seth A. Johnson <sethajoh@umich.edu>
2023-09-12 16:09:37 -0400

    Issue #20 Bots with no user_agent shouldn't cause an error

--- lib/irus_analytics/user_agent_filter.rb
+++ lib/irus_analytics/user_agent_filter.rb
@@ -4,6 +4,7 @@ module IrusAnalytics
   class UserAgentFilter
     class << self
       def filter_user_agent?(user_agent)
+        return true if user_agent.blank?
         ::IrusAnalytics::Configuration.robots.any? {| robot_regex | user_agent.match(robot_regex)}
       end
     end
